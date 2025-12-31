bdc9bf488e9d2ff701f22c16bc9256881d7edead
DabeenKim <81741760+beencoder@users.noreply.github.com>
2025-12-31 19:59:56 +0900

    fix: htmlFor과 id를 유동적으로 결합하여 접근성 에러 해결

--- src/components/home/ListItem.js
+++ src/components/home/ListItem.js
@@ -1,28 +1,33 @@
-import React, {useState} from "react";
 import { FaTrashAlt, FaPen } from "react-icons/fa";
 
-const ListItem = props => { 
-  const { itemId, itemTxt, changeSelected, onInsertToggle, removeItem, checkedItemHandler } = props;
-  const [checked, setChecked] = useState(false);
-
-  const checkHandler = ({ target }) => {
-    setChecked(!checked);
-    checkedItemHandler(target.id, target.checked);
-  };
+const ListItem = ({ item, onRemove, onToggle, onEdit }) => { 
+  const { id, text, checked } = item;
 
   return (
-    <li className="item" id={itemId}>
+    <li className="item">
       <div className="txtWrap">
-        <input className="check" type="checkbox" id={itemId} checked={checked} onChange={(e) => checkHandler(e)} />
-        <label htmlFor={itemId}></label>
-        <p className="itemTxt">{itemTxt}</p>
+        <input 
+          className="check" 
+          type="checkbox" 
+          id={`todo-${id}`} 
+          checked={checked} 
+          onChange={() => onToggle(id)}
+        />
+        <label htmlFor={`todo-${id}`}></label>
+        
+        <p className={`itemTxt ${checked ? 'completed' : ''}`}>
+          {text}
+        </p>
       </div>
+
       <div className="btnWrap">
-        <button className="btn" onClick={() => {
-          changeSelected({id: itemId, text: itemTxt}); onInsertToggle();}}>
+        <button className="btn" onClick={onEdit}>
           <FaPen size="16" color="#454D52" />
         </button>
-        <button className="btn" onClick={() => removeItem(itemId)}><FaTrashAlt size="16" color="#454D52" /></button>
+        
+        <button className="btn" onClick={() => onRemove(id)}>
+          <FaTrashAlt size="16" color="#454D52" />
+        </button>
       </div>
     </li>
   )
